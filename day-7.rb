1fa2d02bb9a981a11354f34f2513626eafb3864d
Joey Poon <joey@joeypoon.com>
2015-05-12 12:15:30 -0500

    Finish day-7 extreme

--- day-7.rb
+++ day-7.rb
@@ -64,7 +64,8 @@ class Game
     end
 
     def display_results
-      puts "Player: #{@player_choice.to_s} vs Computer: #{@computer_choice.to_s} ----- #{check_choices}"
+      puts
+      puts "    * Player: #{@player_choice.to_s} vs Computer: #{@computer_choice.to_s} ----- #{check_choices}"
     end
 end
 
@@ -76,3 +77,61 @@ game.play
 # Instead of asking the player for their choice, have the computer automatically randomly select both the player and computer
 # run the simulation 1000 times
 # Track who which choice wins the most often
+
+class SimulateGame < Game
+
+  def initialize
+    @comp1wins = 0
+    @comp2wins = 0
+    @ties = 0
+    super
+  end
+
+  def simulate number=1000
+    while number > 0
+      randomize_player_choice
+      generate_computer_choice
+      check_choices
+      number = number - 1
+    end
+    display_results
+  end
+
+  private
+
+  def randomize_player_choice
+    @player_choice = @available_choices.sample
+  end
+
+  def check_choices
+    if @player_choice == @computer_choice
+      @ties = @ties + 1
+      "TIE"
+    elsif @choices[@computer_choice.to_sym]["beat_#{@player_choice.downcase}".to_sym]
+      @comp2wins = @comp2wins + 1
+      "COMPUTER 2 WINS"
+    else
+      @comp1wins = @comp1wins + 1
+      "COMPUTER 1 WINS"
+    end
+  end
+
+  def display_results
+    puts
+    puts "    * Computer 1: #{@comp1wins} wins
+    * Computer 2: #{@comp2wins} wins
+    * Ties: #{@ties}"
+  end
+end
+
+puts
+puts "Run simulation? (y/n)"
+action = gets.chomp.downcase
+
+if action == 'y'
+  puts
+  puts "Please enter number of times to run simulation: "
+  number = gets.chomp.to_i
+  simulation = SimulateGame.new
+  simulation.simulate number
+end
