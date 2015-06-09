3ad9bc178b453327eaae4c0a24d79f77add17518
Joey Poon <joey@joeypoon.com>
2015-06-09 13:16:25 -0500

    Finish pagination and photo uploa uploading for photo uploader

---
+++ day-27/photo_uploader/app/controllers/photos_controller.rb
@@ -0,0 +1,30 @@
+class PhotosController < ApplicationController
+
+  before_action do
+    @photos = Photo.all.order('created_at desc').page(params[:page])
+  end
+
+  def index
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new photo_params
+    if @photo.save
+      redirect_to root_path, notice: "Success!"
+    else
+      flash.now[:alert] = 'Fail!'
+      render :new
+    end
+  end
+
+  private
+
+    def photo_params
+      params.require(:photo).permit(:name)
+    end
+
+end
